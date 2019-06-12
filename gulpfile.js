58ffe99649b72cb9b76604b040a953f78fc7cc52
hyeji <wjdgpwl1004@naver.com>
2019-06-12 23:07:44 +0900

    설정최종커밋

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 //걸프 의존성을 여기 씁니다.
 
 gulp.task('default', function(done){
+
+    gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format());
+
     //걸프 작업을 여기 씁니다.
     //노드소스
     gulp.src("es6/**/*.js")
